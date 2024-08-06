0a691dc98671df70ef123a2e3607b0f81f51573f
Eilla-7 <eilafadel2238@gmail.com>
2024-08-06 23:48:03 +0400

    Edit connetcting

--- utils/redis.js
+++ utils/redis.js
@@ -1,46 +1,49 @@
 import redis from 'redis';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
     this.client.on('error', (err) => {
       console.error(`Redis client error: ${err}`);
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
+  async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
         if (err) {
           return reject(err);
         }
         resolve(value);
+        return (value);
       });
     });
   }
 
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.set(key, value, 'EX', duration, (err) => {
         if (err) {
           return reject(err);
         }
         resolve(true);
+        return (true);
       });
     });
   }
 
-  async del (key) {
+  async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err) => {
         if (err) {
           return reject(err);
         }
         resolve(true);
+        return (true);
       });
     });
   }
