6de8e5c18aed2489ef9a4a05db1dce97338d807a
MemanAwad <meman1awad@gmail.com>
2024-08-08 01:11:13 +0300

    task3

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,27 +1,23 @@
-import { checkRedis, checkDb, countUsers, countFiles } from '../utils';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-const AppController = {
-  async getStatus(req, res) {
-    try {
-      const redisAlive = await checkRedis();
-      const dbAlive = await checkDb();
+class AppController {
 
-      res.status(200).json({ redis: redisAlive, db: dbAlive });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
-
-  async getStats(req, res) {
-    try {
-      const usersCount = await countUsers();
-      const filesCount = await countFiles();
-
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
   }
-};
+
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
 
 export default AppController;
