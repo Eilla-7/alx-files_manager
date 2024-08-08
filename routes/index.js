a3aaa6eeea1dfe342162066e12c8fab22f5f05e9
MemanAwad <meman1awad@gmail.com>
2024-08-08 05:30:28 +0300

    hell

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import AppController from '../controllers/AppController.js';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-export default router;
+  router.get('/status', AppController.getStatus);
+  router.get('/stats', AppController.getStats);
+  router.post('/users', UsersController.postNew);
+  router.post('/files', FilesController.postUpload);
+  router.get('/connect', AuthController.getConnect);
+  router.get('/disconnect', AuthController.getDisconnect);
+  router.get('/users/me', UserController.getMe);
+}
+export default controllerRouting;
